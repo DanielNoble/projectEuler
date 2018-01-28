8a4ca7e58b5f67f2df90df995a664e7e9adc02c4
DanielNoble <32727912+DanielNoble@users.noreply.github.com>
2018-01-27 17:36:41 -0800

    Solved problem 3 largest prime factor

--- src/euler/Main.java
+++ src/euler/Main.java
@@ -1,19 +1,24 @@
 package euler;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        long num = 600851475143L;
-
-        System.out.println("Long = " + num);
+        prime();
     }
 
-    public static void prime(){
-     /*   int num = 300425737571.5;
 
-        for(int i = 0; i < 300425737572; i++) */
+    private static void prime(){ // problem 3
+        long num = 125L;
+        long max = (long)Math.floor(Math.sqrt((double)num));
+
+        for(long i = 3; i <= max;){
+            if (i != num && num % i == 0){
+                num /= i;
+            } else {
+                i += 2;
+            }
+        }
+        System.out.println(num);
     }
 }
