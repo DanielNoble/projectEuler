792d52c74eca98138258ca58ddcb9af8b5b50af7
DanielNoble <32727912+DanielNoble@users.noreply.github.com>
2018-01-27 17:07:00 -0800

    Testing IntelliJ github integration

---
+++ src/euler/Main.java
@@ -0,0 +1,19 @@
+package euler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long num = 600851475143L;
+
+        System.out.println("Long = " + num);
+    }
+
+    public static void prime(){
+     /*   int num = 300425737571.5;
+
+        for(int i = 0; i < 300425737572; i++) */
+    }
+}
