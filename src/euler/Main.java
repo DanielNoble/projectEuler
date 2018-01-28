611487987f558aa709ed8a7fa7608e33158aa2fe
DanielNoble <32727912+DanielNoble@users.noreply.github.com>
2018-01-27 17:57:54 -0800

    Solved problem 3 largest prime factor

--- src/euler/Main.java
+++ src/euler/Main.java
@@ -4,9 +4,28 @@ package euler;
 public class Main {
 
     public static void main(String[] args) {
+        fibonacci();
         prime();
     }
 
+    private static void fibonacci() { // problem 2
+        int previous1 = 2;
+        int previous2 = 1;
+        int current = previous1 + previous2;
+        int sum = 2;
+        final int MAX = 4000000;
+        for (; current <= MAX; ) {
+            if (current % 2 == 0) {
+                sum += current;
+            }
+            current = previous1 + previous2;
+            previous2 = previous1;
+            previous1 = current;
+
+        }
+
+        printAnswer(2, sum);
+    }
 
     private static void prime(){ // problem 3
         long num = 125L;
@@ -19,6 +38,10 @@ public class Main {
                 i += 2;
             }
         }
-        System.out.println(num);
+        printAnswer(3, num);
+    }
+
+    private static void printAnswer(int problemNumber, Object answer) {
+        System.out.printf("The answer to problem %d is %s\n", problemNumber, answer);
     }
 }
