379e39718cc11e254ee44401c38e9518b353fab9
DanielNoble <32727912+DanielNoble@users.noreply.github.com>
2018-02-20 16:24:32 -0800

    Solved Problem 5

--- src/euler/Main.java
+++ src/euler/Main.java
@@ -7,9 +7,11 @@ public class Main {
         mult();
         fibonacci();
         prime();
+        palindrome();
+        evenlyDivisible();
     }
 
-    private static void mult() {
+    private static void mult() { // problem 1
         int sum = 0;
         for (int i = 3; i < 1000; i++){
             if (i % 3 == 0 || i % 5 == 0){
@@ -32,7 +34,6 @@ public class Main {
             current = previous1 + previous2;
             previous2 = previous1;
             previous1 = current;
-
         }
 
         printAnswer(2, sum);
@@ -50,6 +51,44 @@ public class Main {
             }
         }
         printAnswer(3, num);
+    }
+
+    private static void palindrome() { // problem 4
+        int largest = 0;
+        for (int i = 100; i < 1000; i++){
+            for (int j = 100; j < 1000; j++){
+                int value = i * j;
+                String current = String.valueOf(value);
+                if (value / 100000 > 0) {
+                    if (current.charAt(5) == current.charAt(0) && current.charAt(4) == current.charAt(1) && current.charAt(3) == current.charAt(2) && value > largest) {
+                        largest = value;
+                    }
+                }
+            }
+        }
+        printAnswer(4, largest);
+    }
+
+    private static void evenlyDivisible(){ // problem 5
+        long val = 21L;
+        int num = 0;
+        while (num==0) {
+            boolean all = true;
+            for (int i = 1; i < 21; i++) {
+                all = true;
+                if (val % i != 0)
+                {
+                    all = false;
+                    break;
+                }
+            }
+            if (all) {
+                num = (int)val;
+            }
+
+            val++;
+        }
+        printAnswer(5,num);
     }
 
     private static void printAnswer(int problemNumber, Object answer) {
